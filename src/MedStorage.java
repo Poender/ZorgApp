0f95deb48a302ec153dd5e1871fd2b0a39a18f1d
Poender <poender@proton.me>
2025-09-15 14:08:56 +0200

    bugfix

--- src/MedStorage.java
+++ src/MedStorage.java
@@ -3,9 +3,11 @@ import java.io.Serializable;
 import java.util.HashMap;
 
 public class MedStorage implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     private HashMap<String, Double> medStorageAttributes = new HashMap<>();
 
+    // Constructor only if using meds for first time, and medstorage.ser is not available
     MedStorage() {
         medStorageAttributes.put("Paracetamol", 100.0);
         medStorageAttributes.put("Diclofenac", 100.0);
